513fbd466d5fbd99a8e21c0c8d328af5c4c0e01f
jnm2 <me@jnm2.com>
2018-07-09 19:55:17 -0400

    Eat CPU

--- BlazorTest.Client/Tests/SampleTest.cs
+++ BlazorTest.Client/Tests/SampleTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Diagnostics;
 
 namespace BlazorTest.Client.Tests
 {
@@ -6,27 +7,40 @@ namespace BlazorTest.Client.Tests
     {
         [Test]
         public static void SampleTest()
-        {
+        {            
             TestContext.WriteLine("Test output");
+            EatCPU(1000);
+            TestContext.WriteLine("More test output");
+            EatCPU(1000);
             Assert.Pass("Pass message");
         }
 
         [Test]
         public static void FailingTest()
         {
+            EatCPU(1000);
             Assert.Fail("Failure message");
         }
 
         [Test]
         public static void Warning()
         {
+            EatCPU(1000);
             Assert.Warn("Warning message");
         }
 
         [Test]
         public static void InconclusiveTest()
         {
+            EatCPU(1000);
             Assert.Inconclusive("Inconclusive message");
+        }
+
+        private static void EatCPU(int milliseconds)
+        {
+            for (var stopwatch = Stopwatch.StartNew(); stopwatch.ElapsedMilliseconds < milliseconds;)
+            {
+            }
         }
     }
 }
